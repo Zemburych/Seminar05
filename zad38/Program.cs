8532fb36143dc6b97a665484a172a3b7a85a26a8
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-04 12:50:25 +0300

    решили задачу 38

--- zad38/Program.cs
+++ zad38/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+// ввод массива
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {    
+        array[i] = random.NextDouble() * 10; // вещественное число от 0 до 10
+        Console.Write("{0,6:F3}", array[i]);
+    }
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max) max = array[i];
+                if (array[i] < min) min = array[i];
+            }
+    Console.WriteLine();        
+// вывод результата     
+Console.WriteLine("max:  " + max);
+Console.WriteLine("min  " + min);
+Console.WriteLine("Разница максимального и минимального: " + "{0,6:F3}", (max - min));
